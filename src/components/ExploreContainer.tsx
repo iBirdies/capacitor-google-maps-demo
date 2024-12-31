6bbe93d0b3c97d575a91c33152cfb4f32d007011
Arnold Plakolli <plakolliarnold@gmail.com>
2024-12-31 17:45:47 +0100

    Added Google Maps for Web, iOS and Android.

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,14 +1,111 @@
 import './ExploreContainer.css';
+import { Geolocation } from '@capacitor/geolocation';
+import { GoogleMap } from '@capacitor/google-maps';
+import { useRef, useEffect, memo, useState, useMemo } from 'react';
+import { isPlatform, IonSpinner } from '@ionic/react';
 
-interface ContainerProps { }
+const ExploreContainer: React.FC = () => {
+  const [state, setState] = useState<'loading' | 'loaded' | 'error'>('loading');
+  const mapRef = useRef<HTMLElement>();
+  let map: GoogleMap;
+  const apiKey = useMemo<string>(() => {
+    if (isPlatform("ios")) {
+      return import.meta.env.VITE_GOOGLE_MAPS_IOS_API_KEY;
+    }
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
+    if (isPlatform("android")) {
+      return import.meta.env.VITE_GOOGLE_MAPS_ANDROID_API_KEY;
+    }
+
+    return import.meta.env.VITE_GOOGLE_MAPS_WEB_API_KEY;
+  }, []);
+
+  const createMap = async () => {
+    if (!mapRef.current) return;
+
+    map = await GoogleMap.create({
+      id: 'main-map',
+      apiKey: apiKey,
+      config: {
+        center: {
+          lat: 0,
+          lng: 0
+        },
+        zoom: 5
+      },
+      element: mapRef.current
+    });
+  };
+
+  const enableCurrentLocation = async (map: GoogleMap) => {
+    await map.enableCurrentLocation(true);
+    const currentLocation = await Geolocation.getCurrentPosition();
+    await map.setCamera({
+      coordinate: {
+        lat: currentLocation.coords.latitude,
+        lng: currentLocation.coords.longitude
+      },
+      zoom: 15
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 500));
+
+        await createMap();
+        setState('loaded');
+
+        try {
+          if (isPlatform('mobile')) {
+            const currentPermissionStatus = await Geolocation.checkPermissions();
+            switch (currentPermissionStatus.location) {
+              case 'prompt':
+              case 'prompt-with-rationale':
+                const newPermissionStatus = await Geolocation.requestPermissions({
+                  permissions: ['location']
+                });
+                if (newPermissionStatus.location === 'granted') {
+                  await enableCurrentLocation(map!);
+                }
+                break;
+              case 'denied':
+                break;
+              case 'granted':
+                await enableCurrentLocation(map!);
+                break;
+            }
+          } else {
+            await enableCurrentLocation(map!); 
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      } catch (error) {
+        console.error(error);
+
+        setState('error');
+      }
+    })();
+  }, []);
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <capacitor-google-map ref={mapRef} id="map" />
+
+      {state === 'loading' && (
+        <div id="info-container">
+          <IonSpinner />
+        </div>
+      )}
+      {state === 'error' && (
+        <div id="info-container">
+          <p style={{ color: 'red' }}>Error</p>
+        </div>
+      )}
     </div>
   );
 };
 
-export default ExploreContainer;
+export default memo(ExploreContainer);
